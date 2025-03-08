e3179b7c344e6bb777653f51eea448f322f1ab2a
DIALLO ABOUBACAR <a.diallo3012@gmail.com>
2025-03-08 16:49:14 +0100

    amélioration de la gestion des articles dans le formulaire d'ajout, avec des ajustements de mise en forme et des vérifications de champs requis

--- src/components/invoice/AddArticleForm.tsx
+++ src/components/invoice/AddArticleForm.tsx
@@ -1,4 +1,5 @@
-import { Article } from "@/src/types";
+import { Article } from '@/src/types';
+import { useEffect } from 'react';
 
 type AddArticleFormProps = {
   articles: Partial<Article>[];
@@ -11,7 +12,7 @@ type AddArticleFormProps = {
   setProductPrice: (articlePrice: string) => void;
   articleDescription: string;
   setProductDescription: (articleDescription: string) => void;
-}
+};
 const AddArticleForm = ({
   articles,
   setArticles,
@@ -22,25 +23,25 @@ const AddArticleForm = ({
   articlePrice,
   setProductPrice,
   articleDescription,
-  setProductDescription
+  setProductDescription,
 }: AddArticleFormProps) => {
-
   const addArticle = () => {
-    if (!articleName || !articlePrice) alert("Veuillez remplir tous les champs");
+    if (!articleName || !articlePrice)
+      alert('Veuillez remplir tous les champs');
 
     if (articleName && articlePrice) {
       const article: Partial<Article> = {
         name: articleName,
         price: parseInt(articlePrice),
-        description: articleDescription
-      }
+        description: articleDescription,
+      };
       setArticles([...articles, article]);
-      setProductName("");
-      setProductQuantity("");
-      setProductPrice("");
-      setProductDescription("");
+      setProductName('');
+      setProductQuantity('');
+      setProductPrice('');
+      setProductDescription('');
     }
-  }
+  };
 
   return (
     <>
@@ -65,48 +66,73 @@ const AddArticleForm = ({
                 </tr>
               </thead>
               <tbody>
-                {
-                  articles.map((article, index) => (
-                    <tr onClick={() => { }} key={index} className="text-xs border-b cursor-pointer hover:bg-gray-200">
-                      <td className="px-6 py-2 text-black">{index + 1}</td>
-                      <td className="px-6 py-2 font-medium text-black whitespace-wrap">
-                        {article.name}
-                        <p className="text-xs text-gray-400">{article.description}</p>
-                      </td>
-                      {/* <td className="px-6 py-2 text-black">
+                {articles.map((article, index) => (
+                  <tr
+                    onClick={() => {}}
+                    key={index}
+                    className="text-xs border-b cursor-pointer hover:bg-gray-200"
+                  >
+                    <td className="px-6 py-2 text-black">{index + 1}</td>
+                    <td className="px-6 py-2 font-medium text-black whitespace-wrap">
+                      {article.name}
+                      <p className="text-xs text-gray-400">
+                        {article.description}
+                      </p>
+                    </td>
+                    {/* <td className="px-6 py-2 text-black">
                         {article.quantity}
                       </td> */}
-                      <td className="px-6 py-2 text-black">
-                        {article.price} GNF
-                      </td>
-                    </tr>
-                  ))
-                }
+                    <td className="px-6 py-2 text-black">
+                      {article.price} GNF
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
         )}
-        {
-          articles && articles.length === 0 && (
-            <p className="text-black font-semibold text-center mt-12">
-              Veillez ajouter un produit ou service
-            </p>
-          )
-        }
+        {articles && articles.length === 0 && (
+          <p className="text-black font-semibold text-center mt-12">
+            Veillez ajouter un produit ou service
+          </p>
+        )}
       </div>
       <hr className="w-full border-gray-50 py-2" />
       <div className="flex flex-col gap-1 px-4 py-2">
         <div className="relative w-full min-w-[200px]">
-          <label htmlFor="article" className="block mb-2 text-sm font-medium text-black">
+          <label
+            htmlFor="article"
+            className="block mb-2 text-sm font-medium text-black"
+          >
             Nom du produit/service
           </label>
-          <input value={articleName} onChange={(e) => setProductName(e.target.value)} type="text" id="article" className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg  block w-full p-2.5" placeholder="Entrez le nom du produit ou service" required />
+          <input
+            value={articleName}
+            onChange={e => setProductName(e.target.value)}
+            type="text"
+            id="article"
+            className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg  block w-full p-2.5"
+            placeholder="Entrez le nom du produit ou service"
+            required
+          />
         </div>
       </div>
       <div className="flex flex-col gap-1 px-4 py-2">
         <div className="relative w-full min-w-[200px]">
-          <label htmlFor="description" className="block mb-2 text-sm font-medium text-black">Description (optionnel)</label>
-          <textarea value={articleDescription} onChange={(e) => setProductDescription(e.target.value)} id="description" className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg  block w-full p-2.5" placeholder="Entrez une description du produit ou service" required />
+          <label
+            htmlFor="description"
+            className="block mb-2 text-sm font-medium text-black"
+          >
+            Description (optionnel)
+          </label>
+          <textarea
+            value={articleDescription}
+            onChange={e => setProductDescription(e.target.value)}
+            id="description"
+            className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg  block w-full p-2.5"
+            placeholder="Entrez une description du produit ou service"
+            required
+          />
         </div>
       </div>
       <div className="flex gap-2 px-4 py-2">
@@ -117,10 +143,21 @@ const AddArticleForm = ({
           <input value={articleQuantity} onChange={(e) => setProductQuantity(e.target.value)} type="number" id="quantity" className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg  block w-full p-2.5" placeholder="Entrez le nom du produit ou service" required />
         </div> */}
         <div className="relative w-full min-w-[200px]">
-          <label htmlFor="price" className="block mb-2 text-sm font-medium text-black">
+          <label
+            htmlFor="price"
+            className="block mb-2 text-sm font-medium text-black"
+          >
             Montant total (GNF)
           </label>
-          <input value={articlePrice} onChange={(e) => setProductPrice(e.target.value)} type="number" id="price" className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg  block w-full p-2.5" placeholder="Entrez le montant total du produit ou service" required />
+          <input
+            value={articlePrice}
+            onChange={e => setProductPrice(e.target.value)}
+            type="number"
+            id="price"
+            className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg  block w-full p-2.5"
+            placeholder="Entrez le montant total du produit ou service"
+            required
+          />
         </div>
       </div>
       <button
@@ -133,6 +170,6 @@ const AddArticleForm = ({
       </button>
     </>
   );
-}
+};
 
 export default AddArticleForm;
--- src/components/invoice/InvoiceForm.tsx
+++ src/components/invoice/InvoiceForm.tsx
@@ -1,49 +1,45 @@
-
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/src/redux/store";
-import { toast } from "react-toastify";
-import InvoiceFormStep from "../InvoiceFormStep";
-import InvoiceInfoForm from "./InvoiceInfoForm";
-import { generateInvoiceId } from "@/src/libs/generateInvoiceId";
-import AddArticleForm from "./AddArticleForm";
-import SuccessAddInvoice from "./SuccessAddInvoice";
-import { createInvoice } from "@/src/services/invoice";
-import { refreshAccount } from "@/src/libs/refreshAccount";
-import { loginReducer } from "@/src/redux/features/authSlice";
-import { StatusEnum } from "@/src/types";
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/src/redux/store';
+import { toast } from 'react-toastify';
+import InvoiceFormStep from '../InvoiceFormStep';
+import InvoiceInfoForm from './InvoiceInfoForm';
+import { generateInvoiceId } from '@/src/libs/generateInvoiceId';
+import AddArticleForm from './AddArticleForm';
+import SuccessAddInvoice from './SuccessAddInvoice';
+import { createInvoice } from '@/src/services/invoice';
+import { refreshAccount } from '@/src/libs/refreshAccount';
+import { loginReducer } from '@/src/redux/features/authSlice';
+import { StatusEnum } from '@/src/types';
 
 type InvoiceFormProps = {
   isEdit: boolean;
   isOpen: boolean;
   onClose: () => void;
 };
-const InvoiceForm = (
-  { isOpen, onClose, isEdit }: InvoiceFormProps
-) => {
+const InvoiceForm = ({ isOpen, onClose, isEdit }: InvoiceFormProps) => {
   const [step, setStep] = useState<number>(1); // 1: Informations générales, 2: Produits/Services, 3: Confirmation
-  const [campaignName, setCampaignName] = useState<string>("")
-  const [errorMessage, setErrorMessage] = useState<string>("");
-  const [invoiceName, setInvoiceName] = useState<string>("");
-  const [invoiceDate, setInvoiceDate] = useState<string>("");
+  const [campaignName, setCampaignName] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [invoiceName, setInvoiceName] = useState<string>('');
+  const [invoiceDate, setInvoiceDate] = useState<string>('');
   const [invoiceTva, setInvoiceTva] = useState<number>(0);
-  const [invoicePaymentMode, setInvoicePaymentMode] = useState<string>("CASH");
-  const [invoicePaymentCondition, setInvoicePaymentCondition] = useState<string>("NOW");
-  const [invoiceRemark, setInvoiceRemark] = useState<string>("");
-  const [selectedClient, setSelectedClient] = useState<string>("");
-  const [invoiceId, setInvoiceId] = useState<string>("");
+  const [invoicePaymentMode, setInvoicePaymentMode] = useState<string>('CASH');
+  const [invoicePaymentCondition, setInvoicePaymentCondition] =
+    useState<string>('NOW');
+  const [invoiceRemark, setInvoiceRemark] = useState<string>('');
+  const [selectedClient, setSelectedClient] = useState<string>('');
+  const [invoiceId, setInvoiceId] = useState<string>('');
 
   // Article
   const [articles, setArticles] = useState<any[]>([]);
-  const [articleName, setProductName] = useState<string>("");
-  const [articleQuantity, setProductQuantity] = useState<string>("");
-  const [articlePrice, setProductPrice] = useState<string>("");
-  const [articleDescription, setProductDescription] = useState<string>("");
+  const [articleName, setProductName] = useState<string>('');
+  const [articleQuantity, setProductQuantity] = useState<string>('');
+  const [articlePrice, setProductPrice] = useState<string>('');
+  const [articleDescription, setProductDescription] = useState<string>('');
 
-  const router = useRouter()
   const auth = useSelector((state: RootState) => state.auth);
-  const clients = auth.loggedAccountInfos?.clients;
   const accountId = auth.loggedAccountInfos?._id;
   const dispatch = useDispatch();
 
@@ -61,14 +57,14 @@ const InvoiceForm = (
       !invoicePaymentMode ||
       !invoicePaymentCondition
     ) {
-      setErrorMessage("Veuillez remplir tous les champs");
+      setErrorMessage('Veuillez remplir tous les champs');
       return;
     }
 
     let amount = 0;
-    if(articles.length > 0) {
-      articles.forEach((article) => {
-        amount += Number(article.price)
+    if (articles.length > 0) {
+      articles.forEach(article => {
+        amount += Number(article.price);
       });
     }
 
@@ -80,86 +76,80 @@ const InvoiceForm = (
       date: invoiceDate,
       paymentMode: invoicePaymentMode,
       paymentCondition: invoicePaymentCondition,
-      status: invoicePaymentCondition === "NOW" ? StatusEnum.PAID : StatusEnum.DRAFT,
+      status:
+        invoicePaymentCondition === 'NOW' ? StatusEnum.PAID : StatusEnum.DRAFT,
       tva: invoiceTva,
       articles,
       amount,
-    }
+    };
 
     if (step === 3) {
-      createInvoice(invoiceToAdd, auth.token!).then(async (response) => {
+      createInvoice(invoiceToAdd, auth.token!).then(async response => {
         if (response.success) {
-          dispatch(loginReducer({ isAuthenticated: true, loggedAccountInfos: response.account }))
-          toast.success("Facture créé avec succès");
+          dispatch(
+            loginReducer({
+              isAuthenticated: true,
+              loggedAccountInfos: response.account,
+            })
+          );
+          toast.success('Facture créé avec succès');
           onClose();
         } else if (!response.success) {
           setErrorMessage(response.message);
         } else {
-          setErrorMessage("Erreur lors de la création de la facture");
+          setErrorMessage('Erreur lors de la création de la facture');
         }
       });
     }
 
-
     if (step > 0 && step < 3) setStep(step + 1);
-  }
-
+  };
 
   if (!isOpen) return null;
   return (
     <>
-      <div
-        className="fixed inset-0 z-[999] grid place-items-center bg-black bg-opacity-60  backdrop-blur-sm transition-opacity duration-300"
-      >
-        <div
-          className="relative m-4 w-2/6 h-[90%] overflow-auto  ] md:rounded-lg bg-white font-sans text-base font-light leading-relaxed text-blue-gray-500 antialiased shadow-2xl"
-        >
+      <div className="fixed inset-0 z-[999] grid place-items-center bg-black bg-opacity-60  backdrop-blur-sm transition-opacity duration-300">
+        <div className="relative m-4 w-2/6 min-h-max overflow-auto  ] md:rounded-lg bg-white font-sans text-base font-light leading-relaxed text-blue-gray-500 antialiased shadow-2xl">
           <div className="w-full flex justify-between items-center px-6 py-4">
-            <h1 className="text-2xl font-semibold">
-              Création de facture
-            </h1>
+            <h1 className="text-2xl font-semibold">Création de facture</h1>
           </div>
           <InvoiceFormStep step={step} />
-          {
-            step === 1 && (
-              <InvoiceInfoForm
-                invoiceName={invoiceName}
-                setInvoiceName={setInvoiceName}
-                invoiceDate={invoiceDate}
-                setInvoiceDate={setInvoiceDate}
-                invoiceTva={invoiceTva}
-                setInvoiceTva={setInvoiceTva}
-                invoicePaymentMode={invoicePaymentMode}
-                setInvoicePaymentMode={setInvoicePaymentMode}
-                invoicePaymentCondition={invoicePaymentCondition}
-                setInvoicePaymentCondition={setInvoicePaymentCondition}
-                invoiceRemark={invoiceRemark}
-                setInvoiceRemark={setInvoiceRemark}
-                selectedClient={selectedClient}
-                setSelectedClient={setSelectedClient}
-                invoiceId={invoiceId}
-                errorMessage={errorMessage}
-                setErrorMessage={setErrorMessage}
-              />
-            )
-          }
+          {step === 1 && (
+            <InvoiceInfoForm
+              invoiceName={invoiceName}
+              setInvoiceName={setInvoiceName}
+              invoiceDate={invoiceDate}
+              setInvoiceDate={setInvoiceDate}
+              invoiceTva={invoiceTva}
+              setInvoiceTva={setInvoiceTva}
+              invoicePaymentMode={invoicePaymentMode}
+              setInvoicePaymentMode={setInvoicePaymentMode}
+              invoicePaymentCondition={invoicePaymentCondition}
+              setInvoicePaymentCondition={setInvoicePaymentCondition}
+              invoiceRemark={invoiceRemark}
+              setInvoiceRemark={setInvoiceRemark}
+              selectedClient={selectedClient}
+              setSelectedClient={setSelectedClient}
+              invoiceId={invoiceId}
+              errorMessage={errorMessage}
+              setErrorMessage={setErrorMessage}
+            />
+          )}
 
-          {
-            step === 2 && (
-              <AddArticleForm
-                articles={articles}
-                setArticles={setArticles}
-                articleName={articleName}
-                setProductName={setProductName}
-                articleQuantity={articleQuantity}
-                setProductQuantity={setProductQuantity}
-                articlePrice={articlePrice}
-                setProductPrice={setProductPrice}
-                articleDescription={articleDescription}
-                setProductDescription={setProductDescription}
-              />
-            )
-          }
+          {step === 2 && (
+            <AddArticleForm
+              articles={articles}
+              setArticles={setArticles}
+              articleName={articleName}
+              setProductName={setProductName}
+              articleQuantity={articleQuantity}
+              setProductQuantity={setProductQuantity}
+              articlePrice={articlePrice}
+              setProductPrice={setProductPrice}
+              articleDescription={articleDescription}
+              setProductDescription={setProductDescription}
+            />
+          )}
 
           {step === 3 && <SuccessAddInvoice onClick={onSubmit} />}
 
@@ -171,25 +161,22 @@ const InvoiceForm = (
               }}
               className="cursor-pointer px-6 py-3 mr-1 font-sans text-xs font-bold text-red-500 uppercase transition-all rounded-lg middle none center hover:bg-red-500/10 active:bg-red-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             >
-              {step === 1 ? "Annuler" : "Précédant"}
+              {step === 1 ? 'Annuler' : 'Précédant'}
             </button>
-            {
-              step < 3 && (
-                <button
-                  type="button"
-                  onClick={() => onSubmit()}
-                  className="cursor-pointer middle none center rounded-lg bg-gradient-to-tr from-green-600 to-green-400 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                >
-                  SUIVANT
-                </button>
-              )
-            }
+            {step < 3 && (
+              <button
+                type="button"
+                onClick={() => onSubmit()}
+                className="cursor-pointer middle none center rounded-lg bg-gradient-to-tr from-green-600 to-green-400 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              >
+                SUIVANT
+              </button>
+            )}
           </div>
         </div>
       </div>
     </>
-
   );
-}
+};
 
 export default InvoiceForm;
